bd680202769a2d030dd554594b9d802e03a2eb44
José Joab <josejoab003@gmail.com>
2025-12-29 09:24:30 -0300

    Finalizando o projeto teste de estagio

--- script/script.js
+++ script/script.js
@@ -1,34 +1,73 @@
-async function buscarUsuarios(){
-    const usuarios = await fetch('https://randomuser.me/api/?results=20&nat=br')
-    try {
-        if(!usuarios.ok){
-            throw new Error('Dados não carregados..')
-        }   
-        const dadosUsers = await usuarios.json()
-        console.log(dadosUsers);
-        
-        return dadosUsers.results.map(user => { 
-            return {
-                name: user.name,
-                email: user.email, 
-                city: user.location .city
+async function buscarUsuarios() {
+    try{
+        const response = await fetch('https://randomuser.me/api/?results=20')
+
+        if(!response.ok){
+            throw new Error('Dados não carreegados...')
+        }
+        const dados = await response.json()
+        console.log(dados);        
+        return dados.results.map(user =>{
+            return{
+                nome: user.name.first,
+                email: user.email,
+                foto: user.picture.medium
             }
         })
-
-    } catch (error) {
-        throw error;
+    
+    } catch(error){
+        console.log(`Erro ao buscar usuários: ${error.message}`)
     }
 }
 
 
-async function init(){
+
+async function initCards(){
     try {
+        const carregando = document.querySelector('.loading')
+        carregando.style.display = 'none'
+
         const dados = await buscarUsuarios()
-        console.log(dados);
+        //Container que vai conter os cards
+        const container = document.querySelector('.cards')
+
+        for(let usuarios of dados){
+
+            //constainer de imagem 
+            const containerImg = document.createElement('div')
+            containerImg.classList.add('container-imagem')
+            containerImg.innerHTML = `<img src="${usuarios.foto}" alt="">`
+            container.appendChild(containerImg) 
+
+            //Container dados 
+            const constainerDados = document.createElement('div')
+            constainerDados.classList.add('dados-usuario')
+
+
+            // Criando H1 - Nome do User
+            const nomeUser = document.createElement('h1')
+            nomeUser.classList.add('nome')
+            nomeUser.textContent = `${usuarios.nome}`
+            constainerDados.appendChild(nomeUser)
+
+            // Crando span para email
+            const span = document.createElement('span')
+            span.classList.add('email')
+            span.textContent = `${usuarios.email}`
+            constainerDados.appendChild(span)
+
+            //Adiconando tudo dentro do containar
+
+            container.appendChild(constainerDados)
+            console.log(usuarios)
+
+        }
+         
+
+
     } catch (error) {
-        console.log(error);
-            
+        console.log(`Erro ao carregar usuários: ${error.message}`);
+        
     }
 }
-
-init()
+initCards() 
